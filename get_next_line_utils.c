b59b16b47d826c6f129871cd3f965bb5cc87baaa
caarlitoos30 <167211404+caarlitoos30@users.noreply.github.com>
2024-10-09 08:56:00 +0200

    Update get_next_line_utils.c

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,1 +1,71 @@
 
+
+void	*ft_memset(void *b, int c, size_t len)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < len)
+	{
+		((char *)b)[i] = (unsigned char)c;
+		i++;
+	}
+	return (b);
+}
+
+void	ft_bzero(void *str, size_t len)
+{
+	ft_memset(str, '\0', len);
+}
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str [i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strchr(const char *str, size_t i)
+
+{
+	while ((*str != '\0') && (*str != (char)i))
+		str++;
+	if (*str == (char)i)
+		return ((char *)str);
+	return (0);
+}
+
+char	*ft_strjoin(const char *s1, const char *s2)
+{
+	char	*str;
+	size_t	str_len;
+	size_t	i;
+
+	str_len = (ft_strlen(s1) + ft_strlen(s2) + 1);
+	str = (char *)malloc(sizeof(char) * str_len);
+	if (!s1 || !s2 || !str)
+		return (0);
+	i = 0;
+	while (*s1)
+		str[i++] = *s1++;
+	while (*s2)
+		str[i++] = *s2++;
+	str[i] = 0;
+	return (str);
+}
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*result;
+
+	result = malloc(count * size);
+	if (!result)
+		return (0);
+	ft_bzero(result, count * size);
+	return (result);
+}
