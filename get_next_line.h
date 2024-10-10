ada8063d19fc8bc2669c4958ad3594c35761e420
caarlitoos30 <167211404+caarlitoos30@users.noreply.github.com>
2024-10-10 22:37:49 +0200

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -1,15 +1,24 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# include <stdlib.h>
-# include <sys/types.h>
-# include <sys/uio.h>
-# include <unistd.h>
 
+//INCLUDES
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <fcntl.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+//GET_NEXT_LINE
 char	*get_next_line(int fd);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strchr(const char *str, size_t i);
-void	ft_bzero(void *str, size_t len);
+
+//GET_NEXT_LINE_UTILS
+int		ft_strlen(char *str);
+char	*ft_strrchr(char *s, int c);
 void	*ft_calloc(size_t count, size_t size);
-size_t	ft_strlen(const char *str);
+char	*ft_strjoin(char *stash, char *buff);
+char	*ft_substr(char *s, int start, int len);
 
 #endif
